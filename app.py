341893ecc9727e82c8b5abe0db152fccc516312a
Camrado <blackandwhite.b17w@gmail.com>
2024-07-23 17:26:15 +0400

    Changed body to query params in get requests

--- app.py
+++ app.py
@@ -15,13 +15,12 @@ edupage.login(os.environ['EDUPAGE_USERNAME'], os.environ['EDUPAGE_PASSWORD'], os
 
 @app.route('/api/timetable-service/v1/timetable-for-date')
 def get_timetable_for_date():
-	data = request.get_json()
-
-	if 'key' not in data or data['key'] != os.environ['PASSWORD_KEY']:
+	key = request.args.get('key')
+	if key is None or key != os.environ['PASSWORD_KEY']:
 		return jsonify({'message': 'Invalid key'}), 401
 
-	timetable_date = datetime.strptime(data['date'], '%Y-%m-%d').date()
-	group = get_group_by_id(edupage, data['group_id'])
+	timetable_date = datetime.strptime(request.args.get('date'), '%Y-%m-%d').date()
+	group = get_group_by_id(edupage, int(request.args.get('group_id')))
 
 	timetable = get_timetable_for_date_util(edupage, group, timetable_date)
 
@@ -29,15 +28,14 @@ def get_timetable_for_date():
 
 @app.route('/api/timetable-service/v1/working-days')
 def get_working_days():
-	data = request.get_json()
-
-	if 'key' not in data or data['key'] != os.environ['PASSWORD_KEY']:
+	key = request.args.get('key')
+	if key is None or key != os.environ['PASSWORD_KEY']:
 		return jsonify({'message': 'Invalid key'}), 401
 
-	start_date = datetime.strptime(data['start_date'], '%Y-%m-%d').date()
-	end_date = datetime.strptime(data['end_date'], '%Y-%m-%d').date()
-	group = get_group_by_id(edupage, data['group_id'])
-	subgroup = data['subgroup']
+	start_date = datetime.strptime(request.args.get('start_date'), '%Y-%m-%d').date()
+	end_date = datetime.strptime(request.args.get('end_date'), '%Y-%m-%d').date()
+	group = get_group_by_id(edupage, int(request.args.get('group_id')))
+	subgroup = int(request.args.get('subgroup'))
 
 	working_days = get_working_days_util(edupage, group, subgroup, start_date, end_date)
 
