7232a168a6d4534a90d47b3954b4706eaae07598
Camrado <blackandwhite.b17w@gmail.com>
2024-07-23 15:49:42 +0400

    Added restriction and changed api routes

--- app.py
+++ app.py
@@ -13,9 +13,13 @@ app = Flask(__name__)
 edupage = EdupageForSkipSmart()
 edupage.login(os.environ['EDUPAGE_USERNAME'], os.environ['EDUPAGE_PASSWORD'], os.environ['EDUPAGE_DOMAIN'])
 
-@app.route('/timetable/timetable-for-date')
+@app.route('/api/timetable-service/v1/timetable-for-date')
 def get_timetable_for_date():
 	data = request.get_json()
+
+	if data['key'] != os.environ['PASSWORD_KEY']:
+		return jsonify({'message': 'Invalid key'}), 401
+
 	timetable_date = datetime.strptime(data['date'], '%Y-%m-%d').date()
 	group = get_group_by_id(edupage, data['group_id'])
 
@@ -23,9 +27,13 @@ def get_timetable_for_date():
 
 	return jsonify(timetable), 200
 
-@app.route('/timetable/working-days')
+@app.route('/api/timetable-service/v1/working-days')
 def get_working_days():
 	data = request.get_json()
+
+	if data['key'] != os.environ['PASSWORD_KEY']:
+		return jsonify({'message': 'Invalid key'}), 401
+
 	start_date = datetime.strptime(data['start_date'], '%Y-%m-%d').date()
 	end_date = datetime.strptime(data['end_date'], '%Y-%m-%d').date()
 	group = get_group_by_id(edupage, data['group_id'])
